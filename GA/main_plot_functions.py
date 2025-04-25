c9165aeba339eb732093b5dadb54aa1262cb2aca
Vladyslav Rudenko <vladyslav.rudenko@proxet.com>
2025-04-26 01:15:24 +0300

    Added functions to plot

--- GA/main_plot_functions.py
+++ GA/main_plot_functions.py
@@ -1,5 +1,15 @@
-from GA.fitness_func import Deb4Function, RastriginFunction
+from GA.fitness_func import (
+    AckleyFunction,
+    Deb2Function,
+    Deb4Function,
+    RastriginFunction,
+)
 
 if __name__ == "__main__":
-    for func in [RastriginFunction, Deb4Function]:
+    for func in [
+        AckleyFunction,
+        RastriginFunction,
+        Deb2Function,
+        Deb4Function,
+    ]:
         func(1).plot()
